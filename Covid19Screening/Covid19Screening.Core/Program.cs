920ea129ce7acf1586c1c61732047722350a939b
Pero1709 <32167797+Pero1709@users.noreply.github.com>
2021-02-14 20:37:58 +0100

    Projekt angelegt

---
+++ Covid19Screening/Covid19Screening.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Covid19Screening.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Covid19Screening/Covid19Screening.ImportConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Covid19Screening.ImportConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
